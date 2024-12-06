d354c56211fa52552ef7bca50956fae19f07b716
RutananaArnold <69462547+RutananaArnold@users.noreply.github.com>
2024-12-06 23:58:27 +0300

    deleted the kitante files folder

--- assets/img/partners/KitanteMedicalCentre_files/robotshide.js
+++
@@ -1,7 +0,0 @@
-// This looks for tags with classes called robothide and replaces them with the variables specified in the data-contents.
-// This file should be disallowed in robots.txt so that the robots don't index certain things like highlighted listings. Mantis#794
-jQuery(function($) {
-    $(".robothide").each(function() {
-        $(this).replaceWith(window[$(this).data("contents")]);
-    });
-});
